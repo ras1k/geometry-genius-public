dbc48e6cbfbe5dccde4b6ff979c82a530116668e
ras1k <rasikabdullah@gmail.com>
2023-03-19 23:39:40 +0600

    new style added

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ function displayData(triangleName, triangleArea){
     tr.innerHTML = `
     <th>${count}</th>
     <td>${triangleName}</td>
-    <td>${triangleArea}</td>
-    <td><button class="btn btn-info">Convert To M</button></td>
+    <td>${triangleArea}<span> cm<sup>2</sup></span></td>
+    <td><button class="btn btn-info">Convert To M<sup>2</sup></button></td>
     `;
     const tbl = document.getElementById('tbl-container')
     tbl.appendChild(tr);
@@ -13,6 +13,30 @@ function removeInput(inp1, inp2){
     inp1.value = '';
     inp2.value = '';
 }
+// Function to generate a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function color (elementId){
+    const card = document.getElementById(elementId);
+    card.addEventListener('mouseover', function(){
+      card.style.backgroundColor = getRandomColor();
+    });
+    card.addEventListener('mouseout', function(){
+      card.style.backgroundColor = 'white';
+    });
+}
+color('triangle-card');
+color('rectangle-card');
+color('parallelogram-card');
+color('rhombus-card');
+color('pentagon-card');
+color('ellipse-card');
 
 let count = 0;
 document.getElementById('triangle-btn').addEventListener('click', function () {
